275ba9f258ff0ea23917742d466e8b5cf4295948
parth2374 <143696865+parth2374@users.noreply.github.com>
2024-07-30 20:32:10 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -13,25 +13,31 @@ export default function Home() {
 
   return (
     <>
-      <header>
-        <div className="wrapper">
-          <div className="logo">
-            <img src='/logo.svg' alt='Logo' />
-          </div>
-          <ul className="nav-area">
-            <li><a href='/'>Home</a></li>
-            <li><a href='/dashboard'>Dashboard</a></li>
-            <li><a href='/'>Services</a></li>
-            <li><a href='/'>Portfolio</a></li>
-            <li><a href='/'>Contact</a></li>
-          </ul>
-        </div>
+      <section className="relative bg-[url(https://images.unsplash.com/photo-1439792675105-701e6a4ab6f0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fGRhcmslMjBiZyUyMHdlYnNpdGV8ZW58MHx8MHx8fDA%3D)] bg-cover bg-center bg-no-repeat text-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-3xl text-center">
+            <h1
+              className="bg-gradient-to-r from-teal-500 via-sky-400 to-blue-300 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl"
+            >
+              Smart AI Interviews
+              <span className="sm:block mt-3"> Perfect Skills & Assess Talent </span>
+            </h1>
 
-        <div className="welcome-text">
-          <h1>Welcome to AI Interview Mocker</h1>
-          <a href='/dashboard'>Start</a>
+            <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
+            Empower your interview process with our AI-driven platform. Students can practice and refine their skills, while HR professionals can evaluate candidates efficiently and effectively.
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 sm:w-auto"
+                href="/dashboard"
+              >
+                Get Started
+              </a>
+            </div>
+          </div>
         </div>
-      </header>
+      </section>
     </>
   );
 }
